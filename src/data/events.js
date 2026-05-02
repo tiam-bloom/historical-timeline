09c19df08581248dda107d12a2449eb3e3814b9f
Tiam <3036293856@qq.com>
2026-05-02 18:55:39 +0800

    refactor: 更新类型和重要等级枚举值
    类型: 政治/军事/经济/文化/外交/科技
    重要等级: 极高/高/中/低

--- src/data/events.js
+++ src/data/events.js
@@ -11,5 +11,5 @@ export const tabs = [
   { id: 'world', label: '世界历史' }
 ];
 
-export const typeOptions = ['全部类型', '政治', '战争', '文化', '科技', '经济', '社会'];
-export const importanceOptions = ['全部等级', '高', '中', '低'];
+export const typeOptions = ['全部类型', '政治', '军事', '经济', '文化', '外交', '科技'];
+export const importanceOptions = ['全部等级', '极高', '高', '中', '低'];
